f84bfe694ddd85eb83c48b6afca2e4037bfa512e
JunitaHariyati <junitahariyati.0717@gmail.com>
2024-12-28 21:52:43 +0700

    added slideshow

--- Project/src/main/java/com/example/demo/controller/GeneralController.java
+++ Project/src/main/java/com/example/demo/controller/GeneralController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GeneralController {
 
-    @GetMapping("/landing")
+    @GetMapping("")
     public String landing() {
         return "General/landingPage";
     }
