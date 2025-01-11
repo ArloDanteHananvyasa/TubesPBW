01994ef4dd462d20666a0d64977bba26ced00b00
RengganaKurniawan <6182201071@student.unpar.ac.id>
2025-01-11 22:35:30 +0700

    add search bar

--- Project/src/main/java/com/example/demo/controller/CustomerController.java
+++ Project/src/main/java/com/example/demo/controller/CustomerController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +20,12 @@ public class CustomerController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/search")
+    @ResponseBody
+    public List<HomePageData> search(@RequestParam String title){
+        return userRepository.searchTitle("%" + title + "%");
+    }
 
     @GetMapping("/homepage")
     public String homepage(Model model) {
